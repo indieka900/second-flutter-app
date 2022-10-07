a12cbe2d055260377966e9da48fdab245a3356e7
indieka900 <indiekaj@gmail.com>
2022-10-07 12:52:03 +0300

    Second flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
+String imagePath1 = 'images/welcome2.png';
+String imagePath2 = 'images/welcome.png';
+String currentPath = 'images/whallpaper2.png';
 
-
-class WelcomePage extends StatelessWidget {
+class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
 
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,13 +42,27 @@ class WelcomePage extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
+            const SizedBox(
+              height: 60.0,
+            ),
+            Image.asset(currentPath),
+            const SizedBox(
+              height: 45,
+            ),
             ElevatedButton(
               style:
                   ElevatedButton.styleFrom(backgroundColor: Colors.brown[600]),
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  if (currentPath == imagePath1) {
+                    currentPath = imagePath2;
+                  } else {
+                    currentPath = imagePath1;
+                  }
+                });
+              },
               child: const Text("CLick"),
             ),
-            Image.asset('images/welcome2.png'),
             //Image.asset('images/welcome2.png'),
           ],
         ),
