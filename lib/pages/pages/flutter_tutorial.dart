76a7a6ccf21999795710e563fb109fcb677c9f66
indieka900 <indiekaj@gmail.com>
2022-10-10 14:00:22 +0300

    Second tutorial

--- lib/pages/pages/flutter_tutorial.dart
+++ lib/pages/pages/flutter_tutorial.dart
@@ -36,97 +36,106 @@ class _ContentState extends State<ContentF> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Image.asset("images/welcome2.png"),
-          Divider(
-            color: Colors.blueAccent,
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.all(8),
-            width: double.infinity,
-            color: Colors.orangeAccent,
-            child: Center(
-              child: Text(
-                "This is a text widget",
-                style: TextStyle(
-                  color: Colors.brown[800],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("images/welcome2.png"),
+            Divider(
+              color: Colors.blueAccent,
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(8),
+              width: double.infinity,
+              color: Colors.orangeAccent,
+              child: Center(
+                child: Text(
+                  "This is a text widget",
+                  style: TextStyle(
+                    color: Colors.brown[800],
+                  ),
                 ),
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  debugPrint('Elevated button clicked');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: isOn ? Colors.brown : Colors.green,
-                ),
-                child: Text("Elevated button"),
-              ),
-              TextButton(
-                onPressed: () {
-                  debugPrint("TextButton was pressed");
-                },
-                child: Text("Text button"),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          OutlinedButton(
-            onPressed: () {
-              debugPrint("OutlinedButton was pressed");
-            },
-            child: Text("Outlined button"),
-          ),
-          GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              print("Row was tapped");
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Icon(
-                  Icons.local_fire_department_sharp,
-                  color: Color.fromARGB(255, 245, 20, 3),
+                ElevatedButton(
+                  onPressed: () {
+                    debugPrint('Elevated button clicked');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: isOn ? Colors.brown : Colors.green,
+                  ),
+                  child: Text("Elevated button"),
                 ),
-                Text(
-                  "This is the Raw widget",
-                  style: TextStyle(color: Colors.redAccent),
+                TextButton(
+                  onPressed: () {
+                    debugPrint("TextButton was pressed");
+                  },
+                  child: Text("Text button"),
                 ),
-                Icon(Icons.local_fire_department_sharp,
-                    color: Color.fromARGB(255, 245, 20, 3)),
               ],
             ),
-          ),
-          Switch(
-            value: isOn,
-            onChanged: (bool newBool) {
-              setState(() {
-                isOn = newBool;
-              });
-            },
-            activeColor: Color.fromARGB(255, 221, 46, 34),
-            //hoverColor: Colors.redAccent,
-          ),
-          Checkbox(
-            value: isChecked,
-            onChanged: (bool? checked) {
-              setState(() {
-                isChecked = checked;
-              });
-            },
-            checkColor: Colors.pink,
-            activeColor: Colors.greenAccent,
-          )
-        ],
+            SizedBox(
+              height: 5,
+            ),
+            OutlinedButton(
+              onPressed: () {
+                debugPrint("OutlinedButton was pressed");
+              },
+              child: Text("Outlined button"),
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                print("Row was tapped");
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Icon(
+                    Icons.local_fire_department_sharp,
+                    color: Color.fromARGB(255, 245, 20, 3),
+                  ),
+                  Text(
+                    "This is the Raw widget",
+                    style: TextStyle(color: Colors.redAccent),
+                  ),
+                  Icon(Icons.local_fire_department_sharp,
+                      color: Color.fromARGB(255, 245, 20, 3)),
+                ],
+              ),
+            ),
+            Switch(
+              value: isOn,
+              onChanged: (bool newBool) {
+                setState(() {
+                  isOn = newBool;
+                });
+              },
+              activeColor: Color.fromARGB(255, 221, 46, 34),
+              //hoverColor: Colors.redAccent,
+            ),
+            Checkbox(
+              value: isChecked,
+              onChanged: (bool? checked) {
+                setState(() {
+                  isChecked = checked;
+                });
+              },
+              checkColor: Colors.pink,
+              activeColor: Colors.greenAccent,
+            ),
+            Image.asset("images/my_image.jpg"),
+            SizedBox(
+              height: 7,
+            ),
+            Divider(
+              color: Color.fromARGB(255, 141, 112, 24),
+            )
+          ],
+        ),
       ),
     );
   }
